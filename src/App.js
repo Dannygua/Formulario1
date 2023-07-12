e6bb3659d9cf13f8f995d00aae23dc75f12b2051
Dannygua <38481086+Dannygua@users.noreply.github.com>
2023-07-12 18:08:37 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,105 @@
+
+import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from "react"
+import SendData from './components/SendData';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+function App() {
+
+  const [usuarios, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  const fetchUserData = (data) => {
+    setLoading(true)
+    fetch("http://localhost:4000/api/Info", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data["soap:Envelope"]["soap:Body"][0].ObtenerDataClickFullResponse[0].ObtenerDataClickFullResult[0]["diffgr:diffgram"][0].NewDataSet[0])
+
+        setLoading(false)
+      })
+  }
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+    fetchUserData(values)
+
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+
+  return (
+    <div className="App">
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Tipo de Cedula"
+          name="tipoDocumento"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Numero de cedula"
+          name="numeroDocumento"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Enviar
+          </Button>
+        </Form.Item>
+      </Form>
+      <SendData loading={loading} usuarios={usuarios} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SendData.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react"
+import { Card } from 'antd';
+
+import { Tabs } from 'antd';
+
+const SendData = ({ loading, usuarios }) => {
+
+    const gridStyle = {
+        width: '25%',
+        textAlign: 'center',
+    };
+
+
+    const gridStyleDatos = {
+        width: '16.6%',
+        textAlign: 'center',
+    };
+
+    if (loading) return "SIN DATOS"
+
+    const onChange = (key) => {
+        console.log(key);
+    };
+
+    const items = [
+        {
+            key: '1',
+            label: `CONOZCA A SU CLIENTE`,
+            children: <p>
+                <p style={{ display: "flex", position: "absolute", paddingLeft: "22%", paddingTop: "25%", fontSize: "13px" }}>{usuarios?.Informacion_x0020_Personal[0]?.Nombres}</p>
+                <p style={{ display: "flex", position: "absolute", paddingLeft: "56%", paddingTop: "23%", fontSize: "13px" }}>{usuarios?.Informacion_x0020_Personal[0]?.Identificacion}</p>
+                <p style={{ display: "flex", position: "absolute", paddingLeft: "41%", paddingTop: "31%", fontSize: "13px" }}>{usuarios?.Informacion_x0020_Personal[0]?.NiveldeInstruccion}</p>
+
+                <img style={{ height: "100%", width: "100%", padding: "0px", margin: "0px" }} src={require('../images/ImgenFormulario1.jpg')} />
+                <p style={{ padding: "0px", margin: "0px" }}>
+                    <img style={{ height: "100%", width: "100%", padding: "0px", margin: "0px" }} src={require('../images/ImageFormulario2.jpg')} />
+                </p>
+            </p>,
+        },
+        {
+            key: '2',
+            label: `SOLICITUD DE CREDITO`,
+            children: <p>
+                <p style={{ display: "flex", position: "absolute", paddingLeft: "22%", paddingTop: "26%", fontSize: "13px" }}>{usuarios?.Informacion_x0020_Personal[0]?.Nombres}</p>
+                <p style={{ display: "flex", position: "absolute", paddingLeft: "58%", paddingTop: "26%", fontSize: "13px" }}>{usuarios?.Informacion_x0020_Personal[0]?.Identificacion}</p>
+                <p style={{ display: "flex", position: "absolute", paddingLeft: "62%", paddingTop: "29%", fontSize: "13px" }}>{usuarios?.Informacion_x0020_Personal[0]?.NiveldeInstruccion}</p>
+
+                <img style={{ height: "100%", width: "100%", padding: "0px", margin: "0px" }} src={require('../images/solicituddecredito1.jpg')} />
+                <img style={{ height: "100%", width: "100%", padding: "0px", margin: "0px" }} src={require('../images/solicituddecredito2.jpg')} />
+            </p>,
+        },
+    ];
+
+
+    return (
+
+        <div>
+            <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+        </div>
+    );
+};
+
+export default SendData;
